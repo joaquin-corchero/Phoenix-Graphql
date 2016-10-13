dde3c5df6e000e27156e1e0813c1fc4cda47b63b
Joaquin Corchero <joaquin.corchero@gmail.com>
2016-10-13 10:52:10 +0100

    Added the user resolver and type

--- phoenix_graphql/web/schema.ex
+++ phoenix_graphql/web/schema.ex
@@ -3,9 +3,17 @@ defmodule PhoenixGraphql.Schema do
   import_types PhoenixGraphql.Schema.Types
 
   query do
+    @desc "Get all the posts"
     field :posts, list_of(:post) do
       resolve &PhoenixGraphql.PostResolver.all/2
     end
+
+    @desc "Get a user of the blog"
+    field :user, type: :user do
+      arg :id, non_null(:id)
+      resolve &PhoenixGraphql.UserResolver.find/2
+    end
+
   end
 
 end
--- phoenix_graphql/web/schema/types.ex
+++ phoenix_graphql/web/schema/types.ex
@@ -1,9 +1,19 @@
 defmodule PhoenixGraphql.Schema.Types do
   use Absinthe.Schema.Notation
 
+  @desc "A blog user"
+  object :user do
+    field :id, :id
+    field :name, :string
+    field :email, :string
+    field :posts, list_of(:post)
+  end
+
+  @desc "A blog post"
   object :post do
     field :id, :id
     field :title, :string
     field :body, :string
+    field :author, :user
   end
 end
