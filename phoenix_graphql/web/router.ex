6fb24b32d02c6a38e497d63ea4d86f8b208483c6
Joaquin Corchero <joaquin.corchero@gmail.com>
2016-10-13 09:21:11 +0100

    Initialized project and added dep

---
+++ phoenix_graphql/web/router.ex
@@ -0,0 +1,26 @@
+defmodule PhoenixGraphql.Router do
+  use PhoenixGraphql.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", PhoenixGraphql do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", PhoenixGraphql do
+  #   pipe_through :api
+  # end
+end
