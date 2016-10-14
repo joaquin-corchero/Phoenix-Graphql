885e7c5d774b5afe5b78e111641f8b106796d633
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2016-10-14 15:56:43 +0100

    Changes

--- web/resolver/post_resolver.ex
+++ web/resolver/post_resolver.ex
@@ -1,9 +1,9 @@
 defmodule PhoenixGraphql.Resolver.Post do
 
   @posts ~s([
-    {"title":"1", "body":"1"},
-    {"title":"2", "body":"2"},
-    {"title":"3", "body":"3"}
+    {"id":"1","title":"1", "body":"1"},
+    {"id":"2","title":"2", "body":"2"},
+    {"id":"3","title":"3", "body":"3"}
   ])
 
   def all() do
--- web/router.ex
+++ web/router.ex
@@ -5,12 +5,19 @@ defmodule PhoenixGraphql.Router do
       plug PhoenixGraphql.Context
   end
 
+  pipeline :api do
+      plug :accepts, ["json"]
+  end
+
   scope "/" do
+    pipe_through :api
+    get "/posts", PhoenixGraphql.PostController, :index
+
     pipe_through :graphql
 
     get "/graphiql", Absinthe.Plug.GraphiQL, schema: PhoenixGraphql.Schema
     post "/graphiql", Absinthe.Plug.GraphiQL, schema: PhoenixGraphql.Schema
 
-    forward "/", Absinthe.Plug, schema: PhoenixGraphql.Schema
+    forward "/api", Absinthe.Plug, schema: PhoenixGraphql.Schema
   end
 end
