ac9d9863c18d657f90ad2eb31b4a51304cf60ada
Joaquin Corchero <joaquin.corchero@gmail.com>
2016-10-27 00:03:49 +0100

    Uncommented tests

--- lib/otp/post_agent_test.exs
+++ lib/otp/post_agent_test.exs
@@ -1,33 +1,27 @@
 defmodule PhoenixGraphql.PostAgentTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case
 
   alias PhoenixGraphql.PostAgent, as: Sut
   alias PhoenixGraphql.GraphQL.PostDto
 
   setup do
+    stop
     {:ok, agent} = Sut.start_link
     {:ok, agent: agent}
   end
 
-  #test "initializes with 3 values" do
-  #  expected = 3
-  #  actual = Enum.count(Sut.get)
+  test "agent works" do
+    expected = 3
+    actual = Enum.count(Sut.get)
 
-  #  assert actual == expected
-  #end
+    assert actual == expected
 
-  #test "a new post can be added" do
-  #  expected = Enum.count(Sut.get) + 1
-  #  Sut.put(%PostDto{id: 100, title: "the title", body: "This is a very long body"} )
-  #  actual = Enum.count(Sut.get)
+    expected = Enum.count(Sut.get) + 1
+    Sut.put(%PostDto{id: 100, title: "the title", body: "This is a very long body"} )
+    actual = Enum.count(Sut.get)
 
-  #  assert actual == expected
-  #end
-
-  #test "agent can be stopped" do
-  #  stop
-  #  assert Sut.get == :error
-  #end
+    assert actual == expected
+  end
 
   defp stop do
     Sut.stop
