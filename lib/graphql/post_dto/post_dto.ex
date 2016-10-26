7d8505b9d23c146b5e4ec433965bb0f44a7cdde0
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2016-10-26 09:43:10 +0100

    Fixed issues with resolver around Poison, added unit tests for resolver

--- lib/graphql/post_dto/post_dto.ex
+++ lib/graphql/post_dto/post_dto.ex
@@ -12,37 +12,31 @@ defmodule PhoenixGraphql.GraphQL.PostDto do
    field :id, :id
    field :title, :string
    field :body, :string
- end
-
- @posts ~s([
-   {"id":"1","title":"1", "body":"1"},
-   {"id":"2","title":"2", "body":"2"},
-   {"id":"3","title":"3", "body":"3"}
- ])
+  end
 
  def resolve(_args, _info, http_client \\HTTPoison) do
    url = "http://localhost:4000/posts"
-   |> http_client.get
+   |> http_client.get!
    |> handle_response
  end
 
- defp handle_response({:ok, %HTTPoison.Response{body: {body}, status_code: 200}}) do
-   IO.inspect "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-   IO.inspect body
-    post = body
-    #|> Poison.decode!(as: [%__MODULE__{}])
-    {:ok, body}
+ defp handle_response(%{body: body, headers: _, status_code: 200}) do
+   posts = body
+   |> Poison.decode!(as: %{"data" => [%__MODULE__{}]})
+   |> Map.fetch!("data")
+   {:ok, posts}
   end
 
-  defp handle_response({:ok, something}) do
-    IO.inspect "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    IO.inspect something
-
-    {:error, "Bad request"}
+  defp handle_response(%{body: _, headers: _, status_code: 404}) do
+    {:error, "Not found"}
   end
 
-  defp handle_response({:error, _}) do
-    {:error, "Something went wrong"}
+  defp handle_response(%{body: body, headers: _, status_code: 500}) do
+    {:error, "Internal server error: #{body}"}
+  end
+
+  defp handle_response(%{body: body, headers: _, status_code: _}) do
+    {:error, "Something wrong happened: #{body}"}
   end
 
   #def find(id) do
--- lib/graphql/post_dto/post_dto_test.exs
+++ lib/graphql/post_dto/post_dto_test.exs
@@ -4,15 +4,55 @@ defmodule PhoenixGraphql.GraphQL.PostDtoTest do
   alias PhoenixGraphql.GraphQL.PostDto, as: Sut
 
   describe "PostDto.resolve/2" do
-    test "Returns dtos struct from source" do
-      json = ~s({"data":[{"title":"1","id":"1","body":"1"},{"title":"2","id":"2","body":"2"},{"title":"3","id":"3","body":"3"}]})
+    @postsUrl "http://localhost:4000/posts"
 
-      url = "http://localhost:4000/posts"
+    test "Returns dtos struct from source when 200 returned" do
+      json = ~s({"data":[{"title":"1","id":"1","body":"1"},{"title":"2","id":"2","body":"2"}]})
+
       http_client = Stubr.stub!(
-        [get!: fn(url) -> json end],
+        [get!: fn(@postsUrl) -> %{body: json, headers: nil, status_code: 200} end],
         [module: HTTPoison]
       )
-      expected = {:ok, Poison.decode!(json, as: [%Sut{}])}
+
+      expected = json
+      |> Poison.decode!(as: %{"data" => [%Sut{}]})
+      |> Map.fetch!("data")
+      actual = Sut.resolve(%{}, %{}, http_client)
+
+      assert actual == {:ok, expected}
+    end
+
+    test "Returns error when 404 status returned" do
+      http_client = Stubr.stub!(
+        [get!: fn(@postsUrl) -> %{body: nil, headers: nil, status_code: 404} end],
+        [module: HTTPoison]
+      )
+
+      expected = {:error, "Not found"}
+      actual = Sut.resolve(%{}, %{}, http_client)
+
+      assert actual == expected
+    end
+
+    test "Returns error when 500 status returned" do
+      http_client = Stubr.stub!(
+        [get!: fn(@postsUrl) -> %{body: "some error", headers: nil, status_code: 500} end],
+        [module: HTTPoison]
+      )
+
+      expected = {:error, "Internal server error: some error"}
+      actual = Sut.resolve(%{}, %{}, http_client)
+
+      assert actual == expected
+    end
+
+    test "Returns error when other status returned" do
+      http_client = Stubr.stub!(
+        [get!: fn(@postsUrl) -> %{body: "another error", headers: nil, status_code: 501} end],
+        [module: HTTPoison]
+      )
+
+      expected = {:error, "Something wrong happened: another error"}
       actual = Sut.resolve(%{}, %{}, http_client)
 
       assert actual == expected
